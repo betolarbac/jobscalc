6a2672a66201387d5aa1ad9febc71d885676f9d6
betolarbac <betocr.dev@gmail.com>
2021-04-14 22:51:09 -0300

    arquivos html modificados para rotas

--- src/serve.js
+++ src/serve.js
@@ -4,18 +4,14 @@ const express = require("express")
 //passo dois executar o express 
 const server = express()
 
-//request, response, resquest é o pedido que começa todo fluxo apartir do get
-//response é responder uma pagina depois do pedido ser feito 
-server.get('/', (request, response) => {
+const routes = require ("./routes")
 
-    //como colocar o index.html rodando no servidor?
-    //existe varias maneiras de acessar os caminhos do projeto no node usamos uma variavel chamada __diname e concatenamos usando o + e o diretorio do arquivos 
-    // depois o servidor esta com a pagina quebrado pq ?
-    //isso acontece pq a rota dos arquivos não foram criadas 
-    return  response.sendFile(__dirname + "/views/index.html")  
-    //não precisamos criar uma rota para cada arquivos podemos configurar o express para criar cada rota automaticamente agora como ??
-    //vamos criar um pasta chamada public para colocar os arquivos publicos 
-})
+//habilitar arquivos statics, como imagem e o css 
+//ele não serve para criar rotas automaticas sempre, ele serve para adicionar configurações ao servidor
+server.use(express.static("public"))
+
+server.use(routes)
+
 
 //agora criamos o servidor usando o objeto listen 
 server.listen(3000, () => console.log ('rodando'))
